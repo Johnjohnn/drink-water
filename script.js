68dc4c333e77743b072078be9bfbb7ecc7d1f9fd
johnjohnn <conde2010@aol.com>
2021-03-27 23:05:14 -0400

    finished

--- script.js
+++ script.js
@@ -4,13 +4,18 @@ const percentage = document.getElementById(".percentage")
 const remained = document.getElementById(".remained")
 
 
+ updateBigcup()
+
+
 smallCups.forEach((cup, idx) => {
     cup.addEventListener("click", () => highlightCups (idx))
 
 })
 
 function highlightCups (idx) {
-
+if (smallCups[idx].classList.contains("full") && !smallCups [idx].nextElementSibling.classList.contains("full")){
+    idx--
+}
 
 
 
@@ -23,4 +28,29 @@ function highlightCups (idx) {
 
 })
 
+
+updateBigcup()
+}
+
+
+function updateBigcup (){
+    const fullCups = document.querySelectorAll(".cup-small.full").length
+
+    const totalCups = smallCups.length
+
+    if (fullCups === 0) {
+        percentage.style.visibility = "hidden" 
+        percentage.style.height = 0
+    }else {
+        percentage.style.visibility = "visible"
+        percentage.style.height = `${fullCups / totalCups * 330}px`
+        percentage.innerText = `${fllCups / totalCups * 100} %`
+    }
+    if(fullCups === totalCups){
+        remained.style.visibility ="hidden"
+        remained.style.height = 0 
+    }else {
+        remained.style.visibility = "visible"
+        liters.innerText = `${2 - (250 * fullCups / 1000)}`
+    }
 }
